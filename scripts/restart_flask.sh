3e31c7945004a760b0ef1bcfe26de4cfa8907169
BilehElmi <elmibileh7@gmail.com>
2025-04-16 18:29:22 +0100

    Update restart_flask.sh

--- scripts/restart_flask.sh
+++ scripts/restart_flask.sh
@@ -3,3 +3,11 @@ cd /home/ec2-user/flask-cicd-demo
 pip3 install -r requirements.txt
 pkill -f "python3 app.py" || true
 nohup python3 app.py > /dev/null 2>&1 &
+
+
+#!/bin/bash
+chmod +x "$0"
+cd /home/ec2-user/flask-cicd-demo
+pip3 install -r requirements.txt
+pkill -f "python3 app.py" || true
+nohup python3 app.py > /dev/null 2>&1 &
